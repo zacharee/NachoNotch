6a5de46b608fdddfdaedd67ceb97bd6adacbf445
zacharee <zachary.wander@gmail.com>
2019-05-26 17:43:04 -0400

    Re-add FLAG_DIM_BEHIND

--- app/src/main/java/com/xda/nachonotch/views/TopOverlay.kt
+++ app/src/main/java/com/xda/nachonotch/views/TopOverlay.kt
@@ -2,18 +2,22 @@ package com.xda.nachonotch.views
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.PixelFormat
+import android.hardware.display.DisplayManagerGlobal
 import android.util.AttributeSet
 import android.view.Gravity
+import android.view.View
 import android.view.WindowManager
-import android.view.WindowManager.LayoutParams.FLAG_DIM_BEHIND
-import android.view.WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR
+import android.view.WindowManager.LayoutParams.*
 import com.xda.nachonotch.util.prefManager
 
 class TopOverlay(context: Context) : BaseOverlay(context, backgroundColor = Color.BLACK) {
     override val params: WindowManager.LayoutParams
         get() = super.params.apply {
+            flags = flags or FLAG_DIM_BEHIND
+            dimAmount = 0f
             gravity = Gravity.TOP
-            width = WindowManager.LayoutParams.MATCH_PARENT
+            width = MATCH_PARENT
             height = context.prefManager.statusBarHeight
         }
 }
