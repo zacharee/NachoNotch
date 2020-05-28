f6c3fc4ee81396befa667c087499e5cb6f78436f
zacharee <zachary.wander@gmail.com>
2020-05-28 19:27:05 -0400

    Implement Unblacklister

--- app/src/main/java/com/xda/nachonotch/App.kt
+++ app/src/main/java/com/xda/nachonotch/App.kt
@@ -12,7 +12,7 @@ import android.view.IRotationWatcher
 import com.xda.nachonotch.util.cachedRotation
 import com.xda.nachonotch.util.refreshScreenSize
 import com.xda.nachonotch.util.rotation
-import java.lang.reflect.Method
+import tk.zwander.unblacklister.disableApiBlacklist
 
 @SuppressLint("PrivateApi")
 class App : Application() {
@@ -33,18 +33,7 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            val forName = Class::class.java.getDeclaredMethod("forName", String::class.java)
-            val getDeclaredMethod = Class::class.java.getDeclaredMethod("getDeclaredMethod", String::class.java, arrayOf<Class<*>>()::class.java)
-
-            val vmRuntimeClass = forName.invoke(null, "dalvik.system.VMRuntime") as Class<*>
-            val getRuntime = getDeclaredMethod.invoke(vmRuntimeClass, "getRuntime", null) as Method
-            val setHiddenApiExemptions = getDeclaredMethod.invoke(vmRuntimeClass, "setHiddenApiExemptions", arrayOf(arrayOf<String>()::class.java)) as Method
-
-            val vmRuntime = getRuntime.invoke(null)
-
-            setHiddenApiExemptions.invoke(vmRuntime, arrayOf("L"))
-        }
+        disableApiBlacklist()
 
         refreshScreenSize()
         cachedRotation = rotation
