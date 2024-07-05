46bb5ab77326b41f903ba38d88b6a18008477ff1
Zachary Wander <zachary.wander@gmail.com>
2024-07-05 19:08:01 -0400

    Update dependencies and migrate to Kotlin 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.bugsnag.android)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
