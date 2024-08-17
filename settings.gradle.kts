869660676b9044646fcc34224efcf89d3c9ca6f6
Zachary Wander <zachary.wander@gmail.com>
2024-08-17 17:21:37 -0400

    Update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,7 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/")
     }
 }
 dependencyResolutionManagement {
@@ -17,6 +18,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven(url = "https://jitpack.io")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/")
     }
 }
 
